1258ae5b8ce7fa73ff71211cf070f1f09a0a1cee
Miles Smith <miles-smith@omrf.org>
2018-09-13 21:06:18 +0000

    Added merging of similar modules

--- R/clusterWGCNA.R
+++ R/clusterWGCNA.R
@@ -5,6 +5,8 @@
 #' @param seuratObj
 #' @param min.module.size
 #' @param filter.mito.ribo.genes
+#' @param assay.use
+#' @param slot.use
 #'
 #' @import dplyr
 #' @importFrom Seurat FetchData WhichCells SetAllIdent SubsetData FindAllMarkers
@@ -12,6 +14,7 @@
 #' @importFrom flashClust flashClust
 #' @importFrom dynamicTreeCut cutreeDynamic
 #' @importFrom stats as.dist
+#' @importFrom glue glue
 #'
 #' @return
 #' @export
@@ -23,17 +26,31 @@
 
 seuratClusterWGCNA <- function(seuratObj,
                                min.module.size = 50,
-                               filter.mito.ribo.genes = FALSE) {
+                               minFraction = 0.25,
+                               filter.mito.ribo.genes = FALSE,
+                               assay.use = NULL,
+                               slot.use = 'data',
+                               merge.similar.modules = FALSE,
+                               merge.similarity.threshold = 0.25) {
 
   options(stringsAsFactors = FALSE)
   WGCNA::allowWGCNAThreads()
 
-  seuratObj.data <- as.matrix(seuratObj@data) %>%
-    dplyr::na_if(y = 0) %>%
-    t()
+  if(!is.null(assay.use)){
+    seuratObj.data <- GetAssayData(seuratObj,
+                                   assay.type = assay.use,
+                                   slot = slot.use) %>%
+      dplyr::na_if(y = 0) %>%
+      t()
+  } else {
+    seuratObj.data <- as.matrix(seuratObj@data) %>%
+      dplyr::na_if(y = 0) %>%
+      t()
+  }
+
   names(seuratObj.data) <- colnames(seuratObj.data)
   gsg <- goodSamplesGenes(seuratObj.data,
-                          minFraction = 0.25,
+                          minFraction = minFraction,
                           verbose = 6)
   datExpr <- seuratObj.data[gsg$goodSamples, gsg$goodGenes]
 
@@ -90,13 +107,15 @@ seuratClusterWGCNA <- function(seuratObj,
     softPower <- which(sft$fitIndices$SFT.R.sq == max(sft$fitIndices$SFT.R.sq))
   }
 
+  print(glue("Using {softPower} for softPower"))
 
+  print(glue("Calculating adjacency matrix..."))
   adj <- adjacency(datExpr,
                    type = "signed",
                    power = softPower,
                    corFnc = "bicor")
 
-  # Turn adjacency into topological overlap
+  print(glue("Turning adjacency into topological overlap..."))
   TOM <- TOMsimilarity(adjMat = adj,
                        TOMType = "signed",
                        verbose = 6,
@@ -105,31 +124,29 @@ seuratClusterWGCNA <- function(seuratObj,
   rownames(TOM) <- colnames(TOM) <- SubGeneNames <- colnames(datExpr)
   dissTOM <- 1 - TOM
 
-  #hierarchical clustering of the genes based on the TOM dissimilarity measure
+  print(glue("Hierarchical clustering of the genes based on the TOM dissimilarity measure..."))
   geneTree <- flashClust(d = as.dist(dissTOM))
 
-  # Module identification using dynamic tree cut
-  minModuleSize <- min.module.size
-
+  print(glue("Module identification using dynamic tree cut..."))
   dynamicMods <- cutreeDynamic(dendro = geneTree,
-                               method = "tree",
+                               method = "hybrid",
                                minClusterSize = minModuleSize,
                                distM = dissTOM,
-                               deepSplit = 2,
+                               deepSplit = 3,
                                pamRespectsDendro = FALSE
   )
 
   dynamicColors <- labels2colors(dynamicMods)
 
-  pDC <- plotDendroAndColors(geneTree,
-                             dynamicColors,
-                             "Dynamic Tree Cut",
-                             dendroLabels = FALSE,
-                             hang = 0.03,
-                             addGuide = TRUE,
-                             guideHang = 0.05,
-                             main = "Gene dendrogram and module colors"
-  )
+  # pdc <- plotDendroAndColors(geneTree,
+  #                            dynamicColors,
+  #                            "Dynamic Tree Cut",
+  #                            dendroLabels = FALSE,
+  #                            hang = 0.03,
+  #                            addGuide = TRUE,
+  #                            guideHang = 0.05,
+  #                            main = "Gene dendrogram and module colors"
+  #                            )
 
   #set the diagonal of the dissimilarity to NA
   diag(dissTOM) <- NA
@@ -137,8 +154,9 @@ seuratClusterWGCNA <- function(seuratObj,
   # Visualize the TOM plot.
   # Raise the dissimilarity matrix to a power
   # to bring out the module structure
-  tmp <- TOMplot(dissTOM ^ 4, geneTree, as.character(dynamicColors))
+  # tmp <- TOMplot(dissTOM ^ 4, geneTree, as.character(dynamicColors))
 
+  print(glue("Assembling modules"))
   module_colors <- unique(dynamicColors)
 
   modules <- lapply(module_colors, function(x) {
@@ -147,17 +165,56 @@ seuratClusterWGCNA <- function(seuratObj,
 
   names(modules) <- module_colors
 
-  plots <- plot_grid(topology.fit.index,
-                     mean.connect,
-                     pDC,
-                     tmp,
-                     ncol = 2
-  )
+  MEList <- moduleEigengenes(expr = datExpr,
+                             colors = dynamicColors,
+                             softPower = softPower)
+  MEs <- MEList$eigengenes
 
-  return(list("plots" = plots,
+  if (isTRUE(merge.similar.modules)){
+    print(glue("Prior to merging, found {length(modules)} modules."))
+    # Calculate dissimilarity of module eigengenes
+    MEDiss <- 1 - WGCNA::bicor(MEs,
+                               use = 'pairwise.complete.obs')
+    # Cluster module eigengenes
+    METree <- hclust(as.dist(MEDiss),
+                    method = "complete")
+
+    # Call an automatic merging function
+    merge <- mergeCloseModules(exprData = datExpr,
+                               colors = dynamicColors,
+                               corFnc = "bicor",
+                               cutHeight = merge.similarity.threshold,,
+                               verbose = 3)
+
+    moduleColors = merge$colors
+    colorOrder = c("grey", standardColors(50));
+    moduleLabels = match(moduleColors, colorOrder)-1;
+    MEs <- merge$newMEs
+
+    mergedModules <- lapply(unique(moduleColors), function(x){
+      colnames(datExpr)[which(moduleColors == x)]
+    })
+
+    names(mergedModules) <- unique(moduleColors)
+
+    mergedModules <- lapply(names(mergedModules), function(x) {
+      stringr::str_remove(string = mergedModules[[x]], pattern = "DCA_")
+    })
+
+    names(mergedModules) <- unique(moduleColors)
+  } else {
+    print(glue("Found {length(modules)} modules."))
+  }
+
+  return(list("plots" = list(topology.fit.index,
+                             mean.connect,
+                             pdc,
+                             tmp
+                             ),
               "modules" = modules,
+              "moduleEigengenes" = MEs,
               "adjacency.matrix" = adj,
               "exprMatrix" = datExpr
-  )
-  )
+              )
+         )
 }
