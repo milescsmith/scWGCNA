8d9ad467dc5f549e12fc054f1bd6e9133ec56609
Miles Smith <miles-smith@omrf.org>
2018-09-14 03:31:50 +0000

    Code style improvements

--- R/clusterWGCNA.R
+++ R/clusterWGCNA.R
@@ -3,24 +3,34 @@
 # Plotting additions and interface with Seurat are from me
 #' @title seuratClusterWGCNA
 #'
-#' @description Perform Weighted Gene Co-expression Network Analysis on scRNAseq data
-#' in a Seurat object
+#' @description Perform Weighted Gene Co-expression Network Analysis on scRNAseq
+#' data in a Seurat object
 #'
 #' @param seuratObj Processed Seurat scRNAseq object
-#' @param min.module.size Minimum number of genes needed to form an expression module. Default: 50.
-#' @param filter.mito.ribo.genes Should mitochondrial and ribosomal genes be removed? Default: FALSE
-#' @param assay.use Instead of using expression data from the obj@data slot, use data stored in the obj@assay slot
-#' @param slot.use Assay data slot to use (i.e. "raw.data", "data", or "scale.data").  Default: 'data'
-#' @param merge.similar.modules Should similar modules be merged?
-#' @param merge.similarity.threshold Dissimilarity (i.e., 1-correlation) cutoff used to determine if modules should be merged.
+#' @param min.module.size Minimum number of genes needed to form an expression
+#' module. Default: 50.
+#' @param filter.mito.ribo.genes Should mitochondrial and ribosomal genes be
+#' removed? Default: FALSE
+#' @param minFraction When determining genes to keep, what is the minimum
+#' fraction of samples a gene must be detected in before being remove?
+#' Default: 0.25
+#' @param assay.use Instead of using expression data from the obj@data slot,
+#' use data stored in the obj@assay slot
+#' @param slot.use Assay data slot to use (i.e. "raw.data", "data", or
+#' "scale.data").  Default: 'data'
+#' @param merge.similar.modules Should similar modules be merged? Default: FALSE
+#' @param merge.similarity.threshold Dissimilarity (i.e., 1-correlation) cutoff
+#' used to determine if modules should be merged. Default: 0.25
 #'
 #' @import dplyr
 #' @importFrom Seurat FetchData WhichCells SetAllIdent SubsetData FindAllMarkers
-#' @importFrom WGCNA pickSoftThreshold adjacency TOMsimilarityFromExpr labels2colors plotDendroAndColors TOMplot bicor
+#' @importFrom WGCNA pickSoftThreshold adjacency TOMsimilarityFromExpr
+#' labels2colors plotDendroAndColors TOMplot bicor
 #' @importFrom flashClust flashClust
 #' @importFrom dynamicTreeCut cutreeDynamic
 #' @importFrom stats as.dist
 #' @importFrom glue glue
+#' @importFrom stringr str_remove
 #'
 #' @return
 #' @export
@@ -35,17 +45,17 @@ seuratClusterWGCNA <- function(seuratObj,
                                minFraction = 0.25,
                                filter.mito.ribo.genes = FALSE,
                                assay.use = NULL,
-                               slot.use = 'data',
+                               slot.use = "data",
                                merge.similar.modules = FALSE,
                                merge.similarity.threshold = 0.25) {
-
   options(stringsAsFactors = FALSE)
   WGCNA::allowWGCNAThreads()
 
-  if(!is.null(assay.use)){
+  if (!is.null(assay.use)) {
     seuratObj.data <- GetAssayData(seuratObj,
-                                   assay.type = assay.use,
-                                   slot = slot.use) %>%
+      assay.type = assay.use,
+      slot = slot.use
+    ) %>%
       dplyr::na_if(y = 0) %>%
       t()
   } else {
@@ -56,59 +66,78 @@ seuratClusterWGCNA <- function(seuratObj,
 
   names(seuratObj.data) <- colnames(seuratObj.data)
   gsg <- goodSamplesGenes(seuratObj.data,
-                          minFraction = minFraction,
-                          verbose = 6)
+    minFraction = minFraction,
+    verbose = 6
+  )
   datExpr <- seuratObj.data[gsg$goodSamples, gsg$goodGenes]
 
   if (isTRUE(filter.mito.ribo.genes)) {
-    datExpr <- datExpr[, grep(pattern = "^MT|RP[LS]|MALAT",
-                              x = colnames(datExpr),
-                              value = TRUE,
-                              invert = TRUE
-    )
-    ]
+    datExpr <- datExpr[, grep(
+      pattern = "^MT|RP[LS]|MALAT",
+      x = colnames(datExpr),
+      value = TRUE,
+      invert = TRUE
+    )]
   }
 
   # Choose a set of soft-thresholding powers
-  powers <- c(c(1:10), seq(from = 10, to=30, by=2))
+  powers <- c(c(1:10), seq(from = 10, to = 30, by = 2))
 
   # Call the network topology analysis function
-  sft <- pickSoftThreshold(data = datExpr,
-                           powerVector = powers,
-                           removeFirst = TRUE,
-                           verbose = 5,
-                           networkType = "signed")
+  sft <- pickSoftThreshold(
+    data = datExpr,
+    powerVector = powers,
+    removeFirst = TRUE,
+    verbose = 5,
+    networkType = "signed"
+  )
 
   # Plot the results:
   # Scale-free topology fit index as a function of the soft-thresholding power
   # Red line corresponds to using an R^2 cut-off
-  topology.fit.index <- ggplot(data = sft$fitIndices,
-                               mapping = aes(x = Power,
-                                             y = -sign(slope) * SFT.R.sq)) +
-    geom_text(mapping = aes(label = Power,
-                            color = "Red")) +
-    labs(x = "Soft Threshold (power)",
-         y = "Scale Free Topology Model Fit, signed R^2",
-         title = "Scale independence") +
+  topology.fit.index <- ggplot(
+    data = sft$fitIndices,
+    mapping = aes(
+      x = Power,
+      y = -sign(slope) * SFT.R.sq
+    )
+  ) +
+    geom_text(mapping = aes(
+      label = Power,
+      color = "Red"
+    )) +
+    labs(
+      x = "Soft Threshold (power)",
+      y = "Scale Free Topology Model Fit, signed R^2",
+      title = "Scale independence"
+    ) +
     geom_hline(aes(yintercept = 0.8, color = "Red")) +
     theme(legend.position = "none")
 
   # Mean connectivity as a function of the soft-thresholding power
-  mean.connect <- ggplot(data = sft$fitIndices,
-                         mapping = aes(x = Power,
-                                       y = mean.k.)) +
-    geom_text(mapping = aes(label = Power,
-                            color = "Red")) +
-    labs(x = "Soft Threshold (power)",
-         y = "Mean Connectivity",
-         title = "Mean Connectivity") +
+  mean.connect <- ggplot(
+    data = sft$fitIndices,
+    mapping = aes(
+      x = Power,
+      y = mean.k.
+    )
+  ) +
+    geom_text(mapping = aes(
+      label = Power,
+      color = "Red"
+    )) +
+    labs(
+      x = "Soft Threshold (power)",
+      y = "Mean Connectivity",
+      title = "Mean Connectivity"
+    ) +
     theme(legend.position = "none")
 
   if (max(sft$fitIndices$SFT.R.sq) >= 0.8) {
     sft.values <- sft$fitIndices %>%
       dplyr::filter(SFT.R.sq >= 0.8)
     softPower <- sft$fitIndices[which(sft$fitIndices$SFT.R.sq ==
-                                        min(sft.values$SFT.R.sq)) - 1, ]$Power
+      min(sft.values$SFT.R.sq)) - 1, ]$Power
   } else {
     softPower <- which(sft$fitIndices$SFT.R.sq == max(sft$fitIndices$SFT.R.sq))
   }
@@ -117,29 +146,34 @@ seuratClusterWGCNA <- function(seuratObj,
 
   print(glue("Calculating adjacency matrix..."))
   adj <- adjacency(datExpr,
-                   type = "signed",
-                   power = softPower,
-                   corFnc = "bicor")
+    type = "signed",
+    power = softPower,
+    corFnc = "bicor"
+  )
 
   print(glue("Turning adjacency into topological overlap..."))
-  TOM <- TOMsimilarity(adjMat = adj,
-                       TOMType = "signed",
-                       verbose = 6,
-                       TOMDenom = "mean")
+  TOM <- TOMsimilarity(
+    adjMat = adj,
+    TOMType = "signed",
+    verbose = 6,
+    TOMDenom = "mean"
+  )
 
   rownames(TOM) <- colnames(TOM) <- SubGeneNames <- colnames(datExpr)
   dissTOM <- 1 - TOM
 
-  print(glue("Hierarchical clustering of the genes based on the TOM dissimilarity measure..."))
+  print(glue("Hierarchical clustering of the genes based on the
+             TOM dissimilarity measure..."))
   geneTree <- flashClust(d = as.dist(dissTOM))
 
   print(glue("Module identification using dynamic tree cut..."))
-  dynamicMods <- cutreeDynamic(dendro = geneTree,
-                               method = "hybrid",
-                               minClusterSize = min.module.size,
-                               distM = dissTOM,
-                               deepSplit = 3,
-                               pamRespectsDendro = FALSE
+  dynamicMods <- cutreeDynamic(
+    dendro = geneTree,
+    method = "hybrid",
+    minClusterSize = min.module.size,
+    distM = dissTOM,
+    deepSplit = 3,
+    pamRespectsDendro = FALSE
   )
 
   dynamicColors <- labels2colors(dynamicMods)
@@ -153,15 +187,15 @@ seuratClusterWGCNA <- function(seuratObj,
   #                            guideHang = 0.05,
   #                            main = "Gene dendrogram and module colors"
   #                            )
-  
-  #set the diagonal of the dissimilarity to NA
+
+  # set the diagonal of the dissimilarity to NA
   # diag(dissTOM) <- NA
 
   # Visualize the TOM plot.
   # Raise the dissimilarity matrix to a power
   # to bring out the module structure
   # tmp <- TOMplot(dissTOM ^ 4, geneTree, as.character(dynamicColors))
-  
+
   print(glue("Assembling modules"))
   module_colors <- unique(dynamicColors)
 
@@ -172,40 +206,44 @@ seuratClusterWGCNA <- function(seuratObj,
   names(modules) <- module_colors
 
   print(glue("Calculating module eigengenes..."))
-  MEList <- moduleEigengenes(expr = datExpr,
-                             colors = dynamicColors,
-                             softPower = softPower)
+  MEList <- moduleEigengenes(
+    expr = datExpr,
+    colors = dynamicColors,
+    softPower = softPower
+  )
   MEs <- MEList$eigengenes
 
-  if (isTRUE(merge.similar.modules)){
+  if (isTRUE(merge.similar.modules)) {
     print(glue("Prior to merging, found {length(modules)} modules."))
     print(glue("Calculate dissimilarity of module eigengenes"))
     MEDiss <- 1 - WGCNA::bicor(MEs,
-                               use = 'pairwise.complete.obs')
+      use = "pairwise.complete.obs"
+    )
     # Cluster module eigengenes
     METree <- hclust(as.dist(MEDiss),
-                     method = "complete")
+      method = "complete"
+    )
 
     print(glue("Merging similar modules..."))
-    merge <- mergeCloseModules(exprData = datExpr,
-                               colors = dynamicColors,
-                               corFnc = "bicor",
-                               cutHeight = merge.similarity.threshold,,
-                               verbose = 3)
+    merge <- mergeCloseModules(
+      exprData = datExpr,
+      colors = dynamicColors,
+      corFnc = "bicor",
+      cutHeight = merge.similarity.threshold,
+      verbose = 3
+    )
 
-    moduleColors = merge$colors
-    colorOrder = c("grey", standardColors(50));
-    moduleLabels = match(moduleColors, colorOrder)-1;
+    moduleColors <- merge$colors
     MEs <- merge$newMEs
 
-    mergedModules <- lapply(unique(moduleColors), function(x){
+    mergedModules <- lapply(unique(moduleColors), function(x) {
       colnames(datExpr)[which(moduleColors == x)]
     })
 
     names(mergedModules) <- unique(moduleColors)
 
     mergedModules <- lapply(names(mergedModules), function(x) {
-      stringr::str_remove(string = mergedModules[[x]], pattern = "DCA_")
+      str_remove(string = mergedModules[[x]], pattern = "DCA_")
     })
 
     names(mergedModules) <- unique(moduleColors)
@@ -214,10 +252,10 @@ seuratClusterWGCNA <- function(seuratObj,
     print(glue("Found {length(modules)} modules."))
   }
 
-  return(list("modules" = modules,
-              "moduleEigengenes" = MEs,
-              "adjacency.matrix" = adj,
-              "exprMatrix" = datExpr
-  )
-  )
+  return(list(
+    "modules" = modules,
+    "moduleEigengenes" = MEs,
+    "adjacency.matrix" = adj,
+    "exprMatrix" = datExpr
+  ))
 }
--- R/plotModules.R
+++ R/plotModules.R
@@ -1,32 +1,40 @@
-#' Title
+#' @title plotModules
+#'
+#' @description Use a bubbleplot to display expression levels and percentage for
+#' genes identified as functioning within a network module.
 #'
 #' @param seuratObj
-#' @param modules
-#' @param ...
+#' @param modules Gene expression modules to plot, as determined by
+#' seuratClusterWGCNA
+#' @param ... Additional arguments to pass to bubbleplot
 #'
 #' @import ggplot2
 #' @import magrittr
+#' @import patchwork
 #' @importFrom seuratBubblePlot bubbleplot
 #' @importFrom gridExtra grid.arrange
+#' @importFrom purrr reduce
 #'
-#' @return
+#' @return ggplot2 plots
 #' @export
 #'
 #' @examples
 plotModules <- function(seuratObj, modules, ...) {
-  modplots = lapply((1:length(modules)),
-                    function(x) {
-                      bp <- bubbleplot(
-                        seuratObj = seuratObj,
-                        genes.plot = modules[[x]],
-                        do.return = TRUE,
-                        ...
-                      ) +
-                        labs(title = names(modules)[[x]]) +
-                        xlab(NULL) +
-                        ylab(NULL) +
-                        theme(legend.position = "none")
-                    })
+  modplots <- lapply(
+    (1:length(modules)),
+    function(x) {
+      bp <- bubbleplot(
+        seuratObj = seuratObj,
+        genes.plot = modules[[x]],
+        do.return = TRUE,
+        ...
+      ) +
+        labs(title = names(modules)[[x]]) +
+        xlab(NULL) +
+        ylab(NULL) +
+        theme(legend.position = "none")
+    }
+  )
 
-  purrr::reduce(modplots, `+`)
+  reduce(modplots, `+`)
 }
